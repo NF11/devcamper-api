2629974fa5b14594926f321c88f6549522c8b964
Nassim Fares <nassim.fares.dz@gmail.com>
2020-08-04 21:56:42 +0100

    config connexion to mongoDB

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-//route files
-const bootcamps = require('./routes/bootcamps.routes');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db.config");
 
 // load config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDB();
+//route files
+const bootcamps = require("./routes/bootcamps.routes");
 
 const app = express();
 
-//mounte dev middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+//mount dev middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-//mounte router
-app.use('/api/v1/bootcamps', bootcamps);
+//mount router
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}! in ${process.env.NODE_ENV} mode`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `App listening on port ${PORT}! in ${process.env.NODE_ENV} mode`.yellow.bold
+  );
+});
+
+//handel unhandled promise reject
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`.red.underline);
+  //close server
+  server.close(() => process.exit(1));
 });
