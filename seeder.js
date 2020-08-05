e245a0746ab4c634166a600e00058609cf327257
Nassim Fares <nassim.fares.dz@gmail.com>
2020-08-05 21:59:12 +0100

    add seeder for imoprt or delete bootcamp

---
+++ seeder.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const Bootcamp = require("./models/Bootcamp.schema");
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("DATA DELETED ...".green.inverse);
+    process.exit();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
