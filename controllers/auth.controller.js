15f1c5b7f4ad1b80185ddce6d4d0f1bc61e859f1
Nassim Fares <nassim.fares.dz@gmail.com>
2020-08-10 07:07:26 +0100

    adding logout route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -31,6 +31,17 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(result, 200, res);
 });
 
+// @desc      Logout current user logged / Clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Public
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).send({ success: true, data: {} });
+});
+
 // @desc      Get current user logged
 // @route     GET /api/v1/auth/me
 // @access    Private
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,12 +7,14 @@ const User = require("../models/User.schema");
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
   const headersAuth = req.headers.authorization;
+  // Set token from Bearer token in header
   if (headersAuth && headersAuth.startsWith("Bearer")) {
     token = headersAuth.split(` `)[1];
   }
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
+  // Set token from cookie
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
   //  Make sure token exists
   if (!token)
     return next(new ErrorResponse("Not authorize to access this route", 401));
