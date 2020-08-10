fb80457b39ef586f5917f6b11046621e5070149c
Nassim Fares <nassim.fares.dz@gmail.com>
2020-08-10 07:36:23 +0100

    adding API Security

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
 const errorHandler = require("./middleware/error.middleware");
@@ -30,6 +36,21 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(fileUpload({ createParentPath: true }));
 app.use(cookieParser());
+app.use(mongoSanitize());
+// Prevent xss attacks
+app.use(xss());
+// ser header security
+app.use(helmet());
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+// Prevent http param pollution
+app.use(hpp());
+// Enable CORS
+app.use(cors());
 
 //mount router
 app.use("/api/v1/auth", auth);
